b10435492b940c75276cf605c6fb4fe5fefe4b9f
AR <agustinaromero7@gmail.com>
2021-10-23 16:39:51 -0300

    ajuste

--- carrito.js
+++ carrito.js
@@ -1,23 +1,29 @@
 let listaCarrito;
-let mostrarCarrito = document.getElementById("carritoCard")
+let imprimirCarrito = document.getElementById("carritoCard")
 let btn = document.getElementById("finalizarCompra")
 
 if (localStorage.getItem("carrito") == null) {
     console.log("Carrito Vacio")
 } else {
     listaCarrito = JSON.parse(localStorage.getItem("carrito"))
+    console.log(listaCarrito)
 }
 
+//mostrar Carrito
 listaCarrito.forEach(element => {
-    mostrarCarrito.innerHTML += `
-    <div class="card col-4" style="width: 18rem;">
+    let index = listaCarrito.indexOf(element);
+    imprimirCarrito.innerHTML += `
+    <div class="card" style="width: 18rem;">
+    <img src="" class="card-img-top" alt="">
     <div class="card-body">
-        <h5 class="card-title">${element.descripcion}</h5>
-        <p class="card-text">${element.precio}</p>
+      <h5 class="card-title">${element.descripcion}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">${element.categoria}</h6>
+      <p class="card-text">$ ${element.precio}</p>
+      <button class="btn btn-primary" onclick="comprar(${index})">Comprar</button>
     </div>
-    </div>
-    `
-});
+  </div>
+`
+})
 
 
 
@@ -25,7 +31,7 @@ const finalizarCompra = () => {
 
     let total = 0
 
-    listaObjetos.forEach(element => {
+    listaCarrito.forEach(element => {
         total += element.precio
     })
 
